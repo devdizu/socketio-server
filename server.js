6ad294d198bee8c467fa946d63762cf257667cd0
Diego Zuniga <devdizu@gmail.com>
2021-04-21 19:19:19 -0500

    Refactor methods

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const server = http.createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 const connectedUsers = [];
+const consoleColors = {
+  blue: "\x1b[36m%s\x1b[0m",
+  yellow: "\x1b[33m%s\x1b[0m",
+  white: "\x1b[47m\x1b[0m",
+  cyan: "\x1b[36m%s\x1b[0m",
+};
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -14,34 +20,42 @@ server.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
+function log(color, ...data) {
+  console.log(consoleColors[color], ...data);
+}
+
 function showConnectedUsers() {
-  console.log("Connected users", connectedUsers);
+  log("cyan", "Connected users", connectedUsers);
+}
+
+function connectUser(socket) {
+  const name = socket.request._query["name"] ?? "A user";
+  const sessionID = socket.id;
+  connectedUsers.push({ name, sessionID });
+  const userConnectedMessage = `${name} has been connected`;
+  io.emit("user connected", userConnectedMessage);
+  return { name, sessionID };
 }
 
 function disconnectUser(sessionID) {
-  connectedUsers.splice(
-    connectedUsers.findIndex((user) => user.sessionID === sessionID),
-    1
+  const userIndex = connectedUsers.findIndex(
+    (user) => user.sessionID === sessionID
   );
+  log("yellow", `${connectedUsers[userIndex].name} has been disconnected`);
+  connectedUsers.splice(userIndex, 1);
 }
 
 io.on("connection", (socket) => {
-  const name = socket.request._query["name"] ?? "A user";
-  const sessionID = socket.id;
-  const userConnectedMessage = `${name} has been connected`;
-  connectedUsers.push({ name, sessionID });
-  console.log("\x1b[36m%s\x1b[0m", userConnectedMessage);
+  const { name, sessionID } = connectUser(socket);
   showConnectedUsers();
-  io.emit("user connected", userConnectedMessage);
 
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    log("white", "message: " + msg);
     io.emit("chat message", `${name}: ${msg}`);
   });
 
   socket.on("disconnect", () => {
     disconnectUser(sessionID);
-    console.log("\x1b[33m%s\x1b[0m", `${name} has been disconnected`);
-    console.log("Connected users", connectedUsers);
+    showConnectedUsers();
   });
 });
